1b5ed3b25377bd429a2308f6605304c7d92f1f9b
mickbrawler <80130864+mickbrawler@users.noreply.github.com>
2025-04-05 14:35:19 +0000

    Deploying to gh-pages from @ mickbrawler/mickbrawler.github.io@c7a2d30d7b1651858f42a07664657e5ed132c13f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "news-began-instruction-at-cornell-sparkles",
+          title: 'Began instruction at Cornell! :sparkles:',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-finished-first-semester-at-cornell-smile",
+          title: 'Finished first semester at Cornell! :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

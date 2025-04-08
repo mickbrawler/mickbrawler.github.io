0194fd17da54367bafbdd505870f272b90012b3b
mickbrawler <80130864+mickbrawler@users.noreply.github.com>
2025-04-08 23:58:45 +0000

    Deploying to gh-pages from @ mickbrawler/mickbrawler.github.io@c73a4fcc1bb59f53ec59d9f5883221a60e28b867 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-mentors",
           title: "mentors",
           description: "",
